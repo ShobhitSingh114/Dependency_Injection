03cf6f7bbf73ef52b3c8233b3ded21eee1ed5f1e
Shobhit <shobhitsingh1149201@gmail.com>
2024-01-01 16:39:44 +0530

    name add manifest, AppModule 2 dependencies of same type, gradle versions, mainActivity viewModel instance added, MyApp Applicaiton class added, MyRepositoryImpl init block, MyViewModel.kt

---
+++ app/src/main/java/com/example/dependencyinjection/MyViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.dependencyinjection
+
+import androidx.lifecycle.ViewModel
+import com.example.dependencyinjection.domain.repository.MyRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyViewModel @Inject constructor(
+    private val repository: MyRepository
+): ViewModel() {
+}
--- app/src/main/java/com/example/dependencyinjection/di/AppModule.kt
+++ app/src/main/java/com/example/dependencyinjection/di/AppModule.kt
@@ -1,11 +1,15 @@
 package com.example.dependencyinjection.di
 
+import android.app.Application
 import com.example.dependencyinjection.data.remote.MyApi
+import com.example.dependencyinjection.data.repository.MyRepositoryImpl
+import com.example.dependencyinjection.domain.repository.MyRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import javax.inject.Named
 import javax.inject.Singleton
 
 //singletonComponent means dependency lives as ling as whole application does
@@ -21,5 +25,29 @@ object AppModule {
             .build()
             .create(MyApi::class.java)
     }
+
+    @Provides
+    @Singleton
+    fun provideMyRepository(
+        api: MyApi,
+        app: Application,
+        @Named("hello1") hello1: String
+    ): MyRepository {
+        return MyRepositoryImpl(api, app)
+    }
+
+    /**
+     * Two dependencies of same type
+     */
+
+    @Provides
+    @Singleton
+    @Named("hello1")
+    fun prividesString1() = "Hello 1"
+
+    @Provides
+    @Singleton
+    @Named("hello2")
+    fun proviesString2() = "Hello 2"
 
 }
